5def7cd639c4dc810eb4c0b908b44a88c51929a1
alinakyrianova <alina.kyrianova@gmail.com>
2023-07-24 21:27:14 +0200

    modal-recipes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './partials/js/modal-recipes';
+fetch('/partials/html/modal-recipes.html') 
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('content').innerHTML = data;
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
 
 
