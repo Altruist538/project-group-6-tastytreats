70fedb2f71b6c81ea84829063d327146f90e411b
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-07-25 00:09:30 +0300

    почав робити функцію фільтрації масива по категоріям рецептів

--- src/partials/js/all-categories.js
+++ src/partials/js/all-categories.js
@@ -1,4 +1,9 @@
+import axios from 'axios';
+
 const url = 'https://tasty-treats-backend.p.goit.global/api/categories';
+
+const btnEl = document.querySelector('.js-all-categories-button');
+const categoriesList = document.querySelector('.categories-list');
 
 fetch(url)
   .then(response => response.json())
@@ -15,3 +20,16 @@ fetch(url)
   .catch(error => {
     console.error(error);
   });
+
+categoriesList.addEventListener('click', onClick);
+
+function onClick(event) {
+  const checkedCategory = event.target.textContent;
+  const filteredRecipes = fetch(
+    'https://tasty-treats-backend.p.goit.global/api/recipes?category=${checkedCategory}&page=1&limit=48'
+  )
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+onClick();
