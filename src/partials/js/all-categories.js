fb452fe0f43eb709e2a4a0ce63ba193da8f373b7
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-07-25 16:40:46 +0300

    All categories (#5)
    * додав скрол
    
    * додав адаптив(розміри)
    
    * почав робити функцію фільтрації масива по категоріям рецептів
    
    * Update all-categories.js
    
    * Update all-categories.js
    
    * Update all-categories.js
    
    * Update all-categories.js
    
    * Додав функцію запиту за категоріями
    
    * Майже вся моя частина готова
    
    ---------
    
    Co-authored-by: Altruist538 <102470985+Altruist538@users.noreply.github.com>

--- src/partials/js/all-categories.js
+++ src/partials/js/all-categories.js
@@ -1,4 +1,10 @@
+import axios from 'axios';
+
 const url = 'https://tasty-treats-backend.p.goit.global/api/categories';
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes';
+
+const btnEl = document.querySelector('.js-all-categories-button');
+const categoriesList = document.querySelector('.categories-list');
 
 fetch(url)
   .then(response => response.json())
@@ -15,3 +21,41 @@ fetch(url)
   .catch(error => {
     console.error(error);
   });
+
+categoriesList.addEventListener('click', getRecipesByCategory);
+
+async function getRecipesByCategory(event) {
+  const buttons = document.querySelectorAll('.all-categories-button');
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => btn.classList.remove('is-active'));
+      button.classList.add('is-active');
+    });
+  });
+
+  const checkedCategory = event.target.textContent;
+
+  try {
+    const { data } = await axios.get(
+      `https://tasty-treats-backend.p.goit.global/api/recipes?category=${checkedCategory}`
+    );
+    return data;
+    console.log(data);
+  } catch (error) {
+    throw new Error('An error occurred while fetching receipes.');
+  }
+  // Додати функцію рендера карток з секції Олександра
+}
+
+btnEl.addEventListener('click', handleAllCategoriesBtnClick);
+
+function handleAllCategoriesBtnClick() {
+  const buttons = document.querySelectorAll('.all-categories-button');
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => btn.classList.remove('is-active'));
+    });
+  });
+
+  // Додати Сашині функцію запиту всіх категорій та рендеру розмітки
+}
