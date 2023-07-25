1d326c2ba0e35049b76bbab1c468078ccef9f87d
AleksandrOgo <ogorodnik.aleksandr@icloud.com>
2023-07-25 11:15:08 +0300

    pictures_backend3

--- src/partials/js/pictures_backend.js
+++ src/partials/js/pictures_backend.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 const galleryEl = document.querySelector('.gallery');
-const myApiKey = '38289805-c9ad3276c7a7da4bbf01374d5';
+// const myApiKey = '38289805-c9ad3276c7a7da4bbf01374d5';
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes';
-const perPage = 40;
+const perPage = 9;
 let pageCounter = 1;
 async function fetchImages() {
   try {
     let response = await axios.get(BASE_URL, {
       params: {
-        key: myApiKey,
+        // key: myApiKey,
         q: '', // Пустой запрос, чтобы получить все картинки при загрузке страницы
         image_type: 'photo',
         orientation: 'horizontal',
@@ -19,37 +19,40 @@ async function fetchImages() {
         per_page: perPage,
       },
     });
-    console.log(response.data);
-    const totalHits = response.data.totalHits;
-    const pagesCount = Math.ceil(totalHits / perPage);
-    if (response.data.hits.length === 0) {
-      console.log('Sorry, there are no images matching your search query. Please try again.');
-    } else {
-      renderImgCard(response.data.hits);
-      if (pagesCount > 1) {
-        buttonLoadMoreEl.style.visibility = 'visible';
-      }
-    }
+      console.log(response.data.results);
+
+      renderImgCard(response.data.results);
+    // const totalHits = response.data.totalHits;
+    // const pagesCount = Math.ceil(totalHits / perPage);
+    // if (response.data.hits.length === 0) {
+    //   console.log('Sorry, there are no images matching your search query. Please try again.');
+    // } else {
+    //   renderImgCard(response.data.hits);
+    //   if (pagesCount > 1) {
+    //     buttonLoadMoreEl.style.visibility = 'visible';
+    //   }
+    // }
   } catch (error) {
     console.log(`Failed to fetch images: ${error}`);
   }
 }
+
 async function renderImgCard(response) {
   let listArr = response.map((resp) => {
     return `<a href="${resp.largeImageURL}" class="gallery__link">
-      <img src="${resp.webformatURL}" alt="${resp.tags}" loading="lazy" />
+      <img src="${resp.preview}" alt="${resp.title}" loading="lazy" />
       <div class="info">
         <p class="info-item">
-          <b>Likes: ${resp.likes}</b>
+          <b>Title: ${resp.title}</b>
         </p>
         <p class="info-item">
-          <b>Views: ${resp.views}</b>
+          <b>Category: ${resp.category}</b>
         </p>
         <p class="info-item">
-          <b>Comments: ${resp.comments}</b>
+          <b>Comments: ${resp.description}</b>
         </p>
-        <p class="info-item">
-          <b>Downloads: ${resp.downloads}</b>
+                <p class="info-item">
+          <b>Area: ${resp.area}</b>
         </p>
       </div>
     </a>`;
