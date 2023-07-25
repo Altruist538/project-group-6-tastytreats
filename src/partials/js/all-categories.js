3932e1386d46636a1db195548685eb34a4016c31
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-07-25 16:09:58 +0300

    Майже вся моя частина готова

--- src/partials/js/all-categories.js
+++ src/partials/js/all-categories.js
@@ -25,7 +25,16 @@ fetch(url)
 categoriesList.addEventListener('click', getRecipesByCategory);
 
 async function getRecipesByCategory(event) {
+  const buttons = document.querySelectorAll('.all-categories-button');
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => btn.classList.remove('is-active'));
+      button.classList.add('is-active');
+    });
+  });
+
   const checkedCategory = event.target.textContent;
+
   try {
     const { data } = await axios.get(
       `https://tasty-treats-backend.p.goit.global/api/recipes?category=${checkedCategory}`
@@ -36,4 +45,17 @@ async function getRecipesByCategory(event) {
     throw new Error('An error occurred while fetching receipes.');
   }
   // Додати функцію рендера карток з секції Олександра
+}
+
+btnEl.addEventListener('click', handleAllCategoriesBtnClick);
+
+function handleAllCategoriesBtnClick() {
+  const buttons = document.querySelectorAll('.all-categories-button');
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => btn.classList.remove('is-active'));
+    });
+  });
+
+  // Додати Сашині функцію запиту всіх категорій та рендеру розмітки
 }
