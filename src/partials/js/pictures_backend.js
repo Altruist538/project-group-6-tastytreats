3a620834209f183379c7c1c970e6963ab7c7e9a5
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-07-25 16:31:22 +0300

    Update pictures_backend.js

--- src/partials/js/pictures_backend.js
+++ src/partials/js/pictures_backend.js
@@ -2,22 +2,21 @@ import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 const galleryEl = document.querySelector('.gallery');
-// const myApiKey = '38289805-c9ad3276c7a7da4bbf01374d5';
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes';
+const windowWidth = document.documentElement.clientWidth;
 let perPage = 0;
 if (windowWidth < 768) {
- perPage= 6;
+  perPage = 6;
 } else if (windowWidth > 768 && windowWidth < 1280) {
   perPage = 8;
 } else if (windowWidth > 1280) {
-  perPage= 9;
+  perPage = 9;
 }
 let pageCounter = 1;
-export function fetchImages() {
+export async function fetchImages() {
   try {
     let response = await axios.get(BASE_URL, {
       params: {
-        // key: myApiKey,
         q: '', // Пустой запрос, чтобы получить все картинки при загрузке страницы
         image_type: 'photo',
         orientation: 'horizontal',
@@ -26,26 +25,16 @@ export function fetchImages() {
         per_page: perPage,
       },
     });
-      console.log(response.data.results);
+    console.log(response.data.results);
 
-      export renderImgCard(response.data.results);
-    // const totalHits = response.data.totalHits;
-    // const pagesCount = Math.ceil(totalHits / perPage);
-    // if (response.data.hits.length === 0) {
-    //   console.log('Sorry, there are no images matching your search query. Please try again.');
-    // } else {
-    //   renderImgCard(response.data.hits);
-    //   if (pagesCount > 1) {
-    //     buttonLoadMoreEl.style.visibility = 'visible';
-    //   }
-    // }
+    renderImgCard(response.data.results);
   } catch (error) {
     console.log(`Failed to fetch images: ${error}`);
   }
 }
 
-async function renderImgCard(response) {
-  let listArr = response.map((resp) => {
+export function renderImgCard(response) {
+  let listArr = response.map(resp => {
     return `<a href="${resp.largeImageURL}" class="gallery__link">
       <img src="${resp.preview}" alt="${resp.title}" loading="lazy" />
       <div class="info">
