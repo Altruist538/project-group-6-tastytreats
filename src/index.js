e282576ec8310a4d18bb69c3f9a9271eafeb30e7
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-07-25 20:52:16 +0300

    hero-slider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import './partials/js/header';
-// import './partials/js/all-categories';
+import './partials/js/header';
+import './partials/js/all-categories';
 import './partials/js/hero-slider';
 import './partials/js/slick.min';
 import './partials/js/order-now';
