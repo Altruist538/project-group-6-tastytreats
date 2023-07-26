996814a92245e9b9572f07f0eb853acf3fef36c3
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-07-26 20:22:20 +0300

    1

--- src/partials/js/all-categories.js
+++ src/partials/js/all-categories.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { fetchImages, renderImgCard } from './pictures_backend';
 
 const url = 'https://tasty-treats-backend.p.goit.global/api/categories';
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes';
@@ -22,8 +23,6 @@ fetch(url)
     console.error(error);
   });
 
-categoriesList.addEventListener('click', getRecipesByCategory);
-
 async function getRecipesByCategory(event) {
   const buttons = document.querySelectorAll('.all-categories-button');
   buttons.forEach(button => {
@@ -35,19 +34,50 @@ async function getRecipesByCategory(event) {
 
   const checkedCategory = event.target.textContent;
 
-  try {
-    const { data } = await axios.get(
-      `https://tasty-treats-backend.p.goit.global/api/recipes?category=${checkedCategory}`
-    );
-    return data;
-    console.log(data);
-  } catch (error) {
-    throw new Error('An error occurred while fetching receipes.');
+  // try {
+  //   const { data } = await axios.get(
+  //     `https://tasty-treats-backend.p.goit.global/api/recipes?category=${checkedCategory}`
+  //   );
+  //   return data;
+  // } catch (error) {
+  //   throw new Error('An error occurred while fetching receipes.');
+  // }
+
+  const windowWidth = document.documentElement.clientWidth;
+  let perPage = 0;
+  if (windowWidth < 768) {
+    perPage = 6;
+  } else if (windowWidth > 768 && windowWidth < 1280) {
+    perPage = 8;
+  } else if (windowWidth > 1280) {
+    perPage = 9;
   }
+  let pageCounter = 1;
+
+  try {
+    let response = await axios.get(
+      `https://tasty-treats-backend.p.goit.global/api/recipes?category=${checkedCategory}`,
+      {
+        params: {
+          q: '', // Пустой запрос, чтобы получить все картинки при загрузке страницы
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          page: pageCounter,
+          per_page: perPage,
+        },
+      }
+    );
+
+    renderImgCard(response.data.results);
+  } catch (error) {
+    console.log(`Failed to fetch images: ${error}`);
+  }
+
   // Додати функцію рендера карток з секції Олександра
 }
 
-btnEl.addEventListener('click', handleAllCategoriesBtnClick);
+categoriesList.addEventListener('click', getRecipesByCategory);
 
 function handleAllCategoriesBtnClick() {
   const buttons = document.querySelectorAll('.all-categories-button');
@@ -59,3 +89,5 @@ function handleAllCategoriesBtnClick() {
 
   // Додати Сашині функцію запиту всіх категорій та рендеру розмітки
 }
+
+btnEl.addEventListener('click', handleAllCategoriesBtnClick);
