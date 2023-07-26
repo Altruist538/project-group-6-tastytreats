cfb58eac0fb69d8e0740c75c94387316cfba435e
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-07-27 00:46:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './partials/js/header';
-//import './partials/js/all-categories';
-//import './partials/js/hero-slider';
+import './partials/js/all-categories';
+import './partials/js/hero-slider';
 import './partials/js/slick.min';
 import './partials/js/order-now';
 import './partials/js/popular-recipes';
