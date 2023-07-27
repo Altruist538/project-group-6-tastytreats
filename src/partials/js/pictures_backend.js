0a5e64b8b7d9121e31843ba82d5d0c0db454d6e7
titova-polina <112263937+titova-polina@users.noreply.github.com>
2023-07-28 01:02:21 +0300

    did task

--- src/partials/js/pictures_backend.js
+++ src/partials/js/pictures_backend.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toggleFavorite } from './favorites';
 const galleryEl = document.querySelector('.gallery');
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes';
 const windowWidth = document.documentElement.clientWidth;
@@ -31,13 +32,9 @@ export async function fetchImages() {
   }
 }
 
-export const getRecipeMarkup = ({
-  preview,
-  title,
-  category,
-  description,
-  area,
-}) => {
+export const getRecipeMarkup = (recipe, favorites = []) => {
+  const { _id, preview, title, category, description, area } = recipe;
+  const isFavorite = favorites.some(fav => fav._id === _id);
   return `<div class="gallery__link">
       <img src="${preview}" class="gallery-img" alt="${title}" loading="lazy" />
       <div class="info">
@@ -53,7 +50,11 @@ export const getRecipeMarkup = ({
         <p class="is-hidden">
           <b> ${area}</b>
         </p>
-        <button class="heart-wrapper">
+        <button class="heart-wrapper ${
+          isFavorite && 'is-active'
+        }" data-heart="${isFavorite}" data-id="${_id}" data-recipe="${encodeURIComponent(
+    JSON.stringify(recipe)
+  )}">
          <svg class="heart-icon" width="22" height="22" viewBox="0 0 32 32">
           <path opacity="0.5" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.9091" d="M15.991 6.848c-2.666-3.117-7.113-3.956-10.451-1.101-3.341 2.854-3.811 7.625-1.188 11.001 2.182 2.806 8.781 8.724 10.944 10.64 0.241 0.214 0.364 0.321 0.505 0.364 0.057 0.017 0.123 0.027 0.191 0.027s0.133-0.010 0.195-0.029l-0.005 0.001c0.141-0.042 0.262-0.15 0.503-0.364 2.164-1.916 8.764-7.834 10.944-10.64 2.624-3.375 2.211-8.177-1.187-11.001-3.398-2.825-7.785-2.016-10.451 1.101z"></path>
          </svg>
@@ -71,12 +72,16 @@ export const getRecipeMarkup = ({
 };
 
 export function renderImgCard(response) {
+  const storage = localStorage.getItem('favorites');
+  const favorites = JSON.parse(storage) || [];
   let listArr = response.map(resp => {
-    return getRecipeMarkup(resp);
+    return getRecipeMarkup(resp, favorites);
   });
   galleryEl.insertAdjacentHTML('beforeend', listArr.join(''));
 }
 // Вызываем функцию fetchImages() при загрузке страницы
 document.addEventListener('DOMContentLoaded', () => {
-  fetchImages();
+  galleryEl && fetchImages();
 });
+
+galleryEl && galleryEl.addEventListener('click', toggleFavorite);
