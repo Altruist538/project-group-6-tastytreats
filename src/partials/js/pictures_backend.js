201ddef9ac224beaff1461ddc2d6087f8522d12f
titova-polina <112263937+titova-polina@users.noreply.github.com>
2023-07-28 02:07:24 +0300

    Favorites (#25)
    * did markup and styles
    
    * did task
    
    * import getRecipeMarkup to render cards
    
    * corrected
    
    * did styles for cards
    
    * did task
    
    * fix errors

--- src/partials/js/pictures_backend.js
+++ src/partials/js/pictures_backend.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toggleFavorite } from './favorites';
 const galleryEl = document.querySelector('.gallery');
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes';
 const windowWidth = document.documentElement.clientWidth;
@@ -27,33 +28,48 @@ export async function fetchImages() {
   }
 }
 
-export const getRecipeMarkup = ({
-  preview,
-  title,
-  category,
-  description,
-  area,
-  rating,
-}) => {
+export const getRecipeMarkup = (recipe, favorites = []) => {
+  const { _id, preview, title, category, description, area, rating } = recipe;
+  const isFavorite = favorites.some(fav => fav._id === _id);
+
   let s1;
   let s2;
   let s3;
   let s4;
   let s5;
 
-
-    let roundedRating = Math.round(rating);
+  let roundedRating = Math.round(rating);
   if (roundedRating === 1) {
-    s1 = 'filled'; s2 = ''; s3 = ''; s4 = ''; s5 = '';
-  } else if(roundedRating === 2){
-    s1 = 'filled';  s2 = 'filled'; s3 = ''; s4 = ''; s5 = '';
-  }else if(roundedRating === 3){
-    s1 = 'filled';  s2 = 'filled'; s3 = 'filled'; s4 = ''; s5 = '';
-  }else if(roundedRating === 4){
-    s1 = 'filled';  s2 = 'filled'; s3 = 'filled'; s4 = 'filled'; s5 = '';
-  }else if(roundedRating === 5){
-    s1 = 'filled';  s2 = 'filled'; s3 = 'filled'; s4 = 'filled'; s5 = 'filled';
-  };
+    s1 = 'filled';
+    s2 = '';
+    s3 = '';
+    s4 = '';
+    s5 = '';
+  } else if (roundedRating === 2) {
+    s1 = 'filled';
+    s2 = 'filled';
+    s3 = '';
+    s4 = '';
+    s5 = '';
+  } else if (roundedRating === 3) {
+    s1 = 'filled';
+    s2 = 'filled';
+    s3 = 'filled';
+    s4 = '';
+    s5 = '';
+  } else if (roundedRating === 4) {
+    s1 = 'filled';
+    s2 = 'filled';
+    s3 = 'filled';
+    s4 = 'filled';
+    s5 = '';
+  } else if (roundedRating === 5) {
+    s1 = 'filled';
+    s2 = 'filled';
+    s3 = 'filled';
+    s4 = 'filled';
+    s5 = 'filled';
+  }
 
   return `<div class="gallery__link">
       <img src="${preview}" class="gallery-img" alt="${title}" loading="lazy" />
@@ -70,11 +86,15 @@ export const getRecipeMarkup = ({
         <p class="is-hidden">
           <b> ${area}</b>
         </p>
-        <button class="heart-wrapper">
+         <button class="heart-wrapper ${
+           isFavorite && 'is-active'
+         }" data-heart="${isFavorite}" data-id="${_id}" data-recipe="${encodeURIComponent(
+    JSON.stringify(recipe)
+  )}">
          <svg class="heart-icon" width="22" height="22" viewBox="0 0 32 32">
           <path opacity="0.5" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.9091" d="M15.991 6.848c-2.666-3.117-7.113-3.956-10.451-1.101-3.341 2.854-3.811 7.625-1.188 11.001 2.182 2.806 8.781 8.724 10.944 10.64 0.241 0.214 0.364 0.321 0.505 0.364 0.057 0.017 0.123 0.027 0.191 0.027s0.133-0.010 0.195-0.029l-0.005 0.001c0.141-0.042 0.262-0.15 0.503-0.364 2.164-1.916 8.764-7.834 10.944-10.64 2.624-3.375 2.211-8.177-1.187-11.001-3.398-2.825-7.785-2.016-10.451 1.101z"></path>
          </svg>
-          </button>
+        </button>
         <div class="wrapper-bottom">
         <div class="rating-wrapper">
         <p class="rating">${rating.toFixed(1)}</p>
@@ -124,12 +144,16 @@ export const getRecipeMarkup = ({
 };
 
 export function renderImgCard(response) {
+  const storage = localStorage.getItem('favorites');
+  const favorites = JSON.parse(storage) || [];
   let listArr = response.map(resp => {
-    return getRecipeMarkup(resp);
+    return getRecipeMarkup(resp, favorites);
   });
   galleryEl.insertAdjacentHTML('beforeend', listArr.join(''));
 }
 // Вызываем функцию fetchImages() при загрузке страницы
 document.addEventListener('DOMContentLoaded', () => {
-  fetchImages();
+  galleryEl && fetchImages();
 });
+
+galleryEl && galleryEl.addEventListener('click', toggleFavorite);
