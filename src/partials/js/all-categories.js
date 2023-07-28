81bbc0144a9780ee1caf2877d849afd6ee416fbd
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-07-28 17:28:05 +0300

    Чистка кода

--- src/partials/js/all-categories.js
+++ src/partials/js/all-categories.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { renderImgCard, fetchImages } from './pictures_backend';
 const galleryEl = document.querySelector('.gallery');
 
-const url = 'https://tasty-treats-backend.p.goit.global/api/categories';
+const url = 'https://tasty-treats-backend.p.goit.global/api/categories/';
 const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes/';
 
 const btnEl = document.querySelector('.js-all-categories-button');
