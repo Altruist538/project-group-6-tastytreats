5ac305d3ba2cc3174cccc78e8847727cf7759685
titova-polina <112263937+titova-polina@users.noreply.github.com>
2023-07-28 13:51:16 +0300

    fix rest pages errors on fav page

--- src/partials/js/render-filters-elements.js
+++ src/partials/js/render-filters-elements.js
@@ -1,27 +1,28 @@
-fetch('https://tasty-treats-backend.p.goit.global/api/areas')
-  .then(response => response.json())
-  .then(data => {
-    const areaFilterEl = document.querySelector('.inp-area-form');
+const areaFilterEl = document.querySelector('.inp-area-form');
+const ingredientsFilterEl = document.querySelector('.inp-ingredients-form');
 
-    data.forEach(area => {
-      const areaOptionEl = document.createElement('option');
-      areaOptionEl.value = area.name;
-      areaOptionEl.textContent = area.name;
-      areaFilterEl.appendChild(areaOptionEl);
-    });
-  })
-  .catch(error => console.log('Ошибка при запросе:', error));
+if (areaFilterEl) {
+  fetch('https://tasty-treats-backend.p.goit.global/api/areas')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(area => {
+        const areaOptionEl = document.createElement('option');
+        areaOptionEl.value = area.name;
+        areaOptionEl.textContent = area.name;
+        areaFilterEl.appendChild(areaOptionEl);
+      });
+    })
+    .catch(error => console.log('Ошибка при запросе:', error));
 
-fetch('https://tasty-treats-backend.p.goit.global/api/ingredients')
-  .then(response => response.json())
-  .then(data => {
-    const ingredientsFilterEl = document.querySelector('.inp-ingredients-form');
-
-    data.forEach(ingredients => {
-      const ingredientsOptionEl = document.createElement('option');
-      ingredientsOptionEl.value = ingredients.name;
-      ingredientsOptionEl.textContent = ingredients.name;
-      ingredientsFilterEl.appendChild(ingredientsOptionEl);
-    });
-  })
-  .catch(error => console.log('Ошибка при запросе:', error));
+  fetch('https://tasty-treats-backend.p.goit.global/api/ingredients')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(ingredients => {
+        const ingredientsOptionEl = document.createElement('option');
+        ingredientsOptionEl.value = ingredients.name;
+        ingredientsOptionEl.textContent = ingredients.name;
+        ingredientsFilterEl.appendChild(ingredientsOptionEl);
+      });
+    })
+    .catch(error => console.log('Ошибка при запросе:', error));
+}
